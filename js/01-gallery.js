b7ec2795e59c3bf57054d080e2a646e0e64cf48e
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2023-07-06 18:04:41 +0200

    gut

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((e) => {
+  let galleryItem = `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${e.preview}"
+        data-source="${e.original}"
+        alt="${e.description}"
+      />
+    </a>
+  </div>`;
+  gallery.insertAdjacentHTML("beforeend", galleryItem);
+});
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  let target = e.target;
+  let bigImage = target.getAttribute("data-source");
+
+  if (bigImage != null) {
+    basicLightbox.create(`<img src="${bigImage}">`).show();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+let galleryElements = [];
+
+galleryItems.forEach((e) => {
+  let listElement = document.createElement("li");
+  let template = `<a class="gallery__item" href="${e.original}">
+      <img
+        class="gallery__image"
+        src="${e.preview}"
+        alt="${e.description}"
+      />
+    </a>`;
+  listElement.insertAdjacentHTML("beforeend", template);
+  galleryElements.push(listElement);
+});
+gallery.append(...galleryElements);
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
